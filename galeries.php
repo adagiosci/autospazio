61f84f4b19b6459bd827c1870c0da0e717b7a5e7
lmartinez <lmartinez@Madagascar>
2014-05-27 11:00:02 -0500

    galeria

--- galeries.php
+++ galeries.php
@@ -8,15 +8,27 @@
 <div class='container-galleries'>
 	<h1>GALERÍAS</h1>
 	<div class='dot-points'></div>
-	<form id="busca" name="busca" method="post" action="tienda">
-		<label for="etiqueta">Tag a buscar</label>
-		<input type="text" name="etiqueta" id="etiqueta"/>
-		<input type="submit" name="buscar" id="buscar" value="Buscar" />
+	<div class="select-form">
+	<form  name="busca" method="post" action="?page_id=9">
+		<?php
+		global $wpdb;
+		$customers = $wpdb->get_results("SELECT gid,name FROM wp_ngg_gallery;");
+		/*print_r($customers);*/
+		echo "<select name='select' onchange='setFormAction(this)'>";
+		foreach ($customers as $customer){
+			echo "<option value = '".$customer->name."'>".$customer->name."</option>"; 
+			/*echo $customer->gid;
+			echo $customer->name;*/
+		}
+		echo "</select>";
+		?>
+	<input type="text" name="idDsn" maxlength="9" readonly size="70" value='<?php echo $customer->gid;?>'>
 	</form>
-	<?php $variable=$_POST['etiqueta']; ?>
 	
-	<?php 
-	echo do_shortcode('[nggtags album="'.$variable.'" override_thumbnail_settings="1" thumbnail_width="240" thumbnail_height="180" thumbnail_crop="1" show_slideshow_link="0"]');
+	</div>
+		
+	<?php /*se pone el shortcode de nextgen para hacer el filtrado por tag*/
+	echo do_shortcode('[nggallery id='.$customer->gid.' override_thumbnail_settings="1" thumbnail_width="240" thumbnail_height="180" thumbnail_crop="1" show_slideshow_link="0"]');
 	?>
 </div>
 <div class='clear'></div>
