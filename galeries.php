5f08549ab2857d229b5f09f35a2c2f71c1c977cd
lmartinez <lmartinez@Madagascar>
2014-05-30 14:01:04 -0500

    galeria

--- galeries.php
+++ galeries.php
@@ -8,12 +8,13 @@
 document.oncontextmenu = function(){return false} 
 </script>
 <?php get_header(); ?>
+
 <div id='gallery'>
 <div class='container-galleries'>
 	<h1>GALERÍAS</h1>
 	<div class='dot-points'></div>
-	<div class="select-form"> <!-- en action para produccion se pone /galeria/ o el nombre de la pagina que usa este template -->
-	<form  method="post" action="/galeria/">
+	<div> <!-- en action para produccion se pone /galeria/ o el nombre de la pagina que usa este template -->
+	<form  method="post" action="?page_id=9">
 		<?php /*aqui hacemos la consulta de las galerias y las visualizamos en un combobox*/
 		global $wpdb;
 		$customers = $wpdb->get_results("SELECT gid,name FROM wp_ngg_gallery;");
@@ -35,14 +36,24 @@ document.oncontextmenu = function(){return false}
 		echo "</select>";
 		?>
 		<input type='submit' value='Buscar'/>
+		<!-- inicio filtro por tags -->
+	    <input type="text" name='etiqueta'></br>
+		<input type='submit' value='Buscar'/>
+		<!-- fin filtro por tags -->
 	</form>
 	
 	</div>
+	
+	
+	
+
+<div>
 	<?php	/*aqui recibimos la opcion del combobox y hacemos la busqueda para sacar el id*/
 	global $wpdb;
 	
 	/*se hace una verificación si se presionó el boton IR entra al if sino se va al else*/
-	if(!empty($_POST['select'])){
+	
+		if(!empty($_POST['select'])){
     /*.. aqui se hace la vista segun la selección de la galería... */
     $select = $_POST['select'];
     $galeria = $wpdb->get_results("SELECT gid,name FROM wp_ngg_gallery WHERE name='$select';");
@@ -56,9 +67,14 @@ document.oncontextmenu = function(){return false}
 		/*se crea la vista segun la galeria seleccionada por el usuario*/
      echo do_shortcode('[nggallery id='.$mostrar.' override_thumbnail_settings="1" thumbnail_width="240" thumbnail_height="180" thumbnail_crop="1" show_slideshow_link="0"]');
 } else {/*en caso contrario se genera la galeria de todas las imagenes*/
-	echo do_shortcode('[nggtags gallery= override_thumbnail_settings="1" thumbnail_width="240" thumbnail_height="180" thumbnail_crop="1" show_slideshow_link="0"]');
+	$etiquetita = $_POST['etiqueta']; /*aqui recibimos la etiqueta a buscar del form etiqueta y se la pasamos a nggtags para que genere la galeria*/
+	echo do_shortcode('[nggtags gallery='.$etiquetita.' override_thumbnail_settings="1" thumbnail_width="240" thumbnail_height="180" thumbnail_crop="1" show_slideshow_link="0"]');
 	}
+		
 ?>
+</div> 
+
+
 </div>
 <div class='clear'></div>
 </div>
