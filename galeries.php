c948d1bb158874c5fe7dbcc04329f08f14fce9fc
Irving Sulub <isulub@zaviatravel.com>
2014-06-02 19:45:53 -0500

    chamges

--- galeries.php
+++ galeries.php
@@ -66,16 +66,16 @@
 
 		/*se crea la vista segun la galeria seleccionada por el usuario*/
 		if($pos !== False){
-     			echo do_shortcode('[nggallery template="autospazio" ngg_force_update=23 id='.$mostrar.' override_thumbnail_settings="1" thumbnail_width="174" thumbnail_height="174" thumbnail_crop="1" show_slideshow_link="0"]');
+     			echo do_shortcode('[nggallery template="autospazio" ngg_force_update=24 id='.$mostrar.' override_thumbnail_settings="1" thumbnail_width="174" thumbnail_height="174" thumbnail_crop="1" show_slideshow_link="0"]');
      		}else{
-     			echo do_shortcode('[album template="autospazio" ngg_force_update=3]﻿');
+     			echo do_shortcode('[album template="autospazio" ngg_force_update=7]﻿');
      		}
 } else {/*en caso contrario se genera la galeria de todas las imagenes*/
 	$etiquetita = $_GET['tag']; /*aqui recibimos la etiqueta a buscar del form etiqueta y se la pasamos a nggtags para que genere la galeria*/
 	if($pos !== False){
-		echo do_shortcode('[nggtags template="autospazio" ngg_force_update=23 gallery="'.$etiquetita.'" override_thumbnail_settings="1" thumbnail_width="174" thumbnail_height="174" thumbnail_crop="1" show_slideshow_link="0"]');	
+		echo do_shortcode('[nggtags template="autospazio" ngg_force_update=24 gallery="'.$etiquetita.'" override_thumbnail_settings="1" thumbnail_width="174" thumbnail_height="174" thumbnail_crop="1" show_slideshow_link="0"]');	
 	}else{
-		echo do_shortcode('[album template="autospazio" ngg_force_update=3]﻿');
+		echo do_shortcode('[album template="autospazio" ngg_force_update=7]﻿');
 	}
 	}
 		
